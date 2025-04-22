1f9436ea239541153b6a3ba1c726e07d2c2e1e2a
Daniel Bryars <bryars@hotmail.com>
2025-04-22 21:53:35 +0100

    WIP

--- 00_train_w2v_embed.py
+++ 00_train_w2v_embed.py
@@ -1,14 +1,18 @@
+import pickle
+
 import gensim.downloader as api
+from gensim.models import KeyedVectors 
 import numpy as np
 import pandas as pd
-import pickle
+from tqdm import tqdm
+from numpy.typing import NDArray
 
 model = api.load("word2vec-google-news-300")
 
-def text_to_embedding(text, model):
+def text_to_embedding(text: str, model:KeyedVectors) -> NDArray[np.float32]:
     words = text.lower().split()
     vectors = [model[word] for word in words if word in model]
-    return np.mean(vectors, axis=0) if vectors else np.zeros(model.vector_size)
+    return np.mean(vectors, axis=0) if vectors else np.zeros(model.vector_size) # type: ignore
 
 #test-00000-of-00001.parquet
 #train-00000-of-00001.parquet
@@ -22,8 +26,8 @@ def compute_embeddings(filename):
       qid = row['query_id']
       query = row['query']
       positive_matches = row['passages']['passage_text']
-      query_vectors[qid] = text_to_embedding(query, model)    
-      passage_vectors[qid] = np.array([text_to_embedding(passage, model) for passage in positive_matches])
+      query_vectors[qid] = text_to_embedding(query, model)     # type: ignore
+      passage_vectors[qid] = np.array([text_to_embedding(passage, model) for passage in positive_matches]) # type: ignore
 
   return query_vectors, passage_vectors
 
