fa6cbc35554629a9a8e3a41eeee5783abfb70a6d
Daniel Bryars <bryars@hotmail.com>
2025-04-23 10:43:11 +0100

    hf req

--- dataset.py
+++ dataset.py
@@ -1,13 +1,28 @@
 import torch
 import pickle
-
+import tempfile
+import os
+import torch
+import pickle
+from huggingface_hub import hf_hub_download
+from pathlib import Path
 
 #https://huggingface.co/datasets/microsoft/ms_marco
 
-
-
+def download_pickles_from_hugginface(filenames = [
+   "test-00000-of-00001.parquet.triplet.embeddings.pkl", 
+   "train-00000-of-00001.parquet.triplet.embeddings.pkl",
+   "validation-00000-of-00001.parquet.triplet.embeddings.pkl"]):
+    for filename in filenames:
+      if not Path("myfile.pkl").exists():
+        hf_hub_download(
+          repo_id="danbhf/two-towers",
+          repo_type="dataset",  # must match what you used during upload
+          filename=filename)
+        
 class TripletDataset(torch.utils.data.Dataset):
     def __init__(self, path_to_pickle):
+        
         data = pickle.load(open(path_to_pickle, 'rb'))
         self.q = data["query"]
         self.p = data["pos"]
@@ -23,34 +38,42 @@ class TripletDataset(torch.utils.data.Dataset):
             torch.tensor(self.n[idx], dtype=torch.float32)
         )
 
-
-class Wiki(torch.utils.data.Dataset):
-  def __init__(self):
-    self.vocab_to_int = pickle.load(open('./tkn_words_to_ids.pkl', 'rb'))
-    self.int_to_vocab = pickle.load(open('./tkn_ids_to_words.pkl', 'rb'))
-    self.corpus = pickle.load(open('./corpus.pkl', 'rb'))
-    self.tokens = [self.vocab_to_int[word] for word in self.corpus]
-
-  def __len__(self):
-    return len(self.tokens)
-
-  def __getitem__(self, idx: int):
-    ipt = self.tokens[idx]
-    prv = self.tokens[idx-2:idx]
-    nex = self.tokens[idx+1:idx+3]
-    if len(prv) < 2: prv = [0] * (2 - len(prv)) + prv
-    if len(nex) < 2: nex = nex + [0] * (2 - len(nex))
-    return torch.tensor(prv + nex), torch.tensor([ipt])
-
-
-#
-#
-#
 if __name__ == '__main__':
-  ds = Wiki()
-  print(ds.tokens[:15])
-  # print(ds[0])
-  print(ds[5])
-  dl = torch.utils.data.DataLoader(dataset=ds, batch_size=3)
-  ex = next(iter(dl))
-  print(ex)
+  def create_dummy_pickle():
+    dummy_data = {
+        "query": [[1.0, 2.0], [3.0, 4.0]],
+        "pos": [[1.1, 2.1], [3.1, 4.1]],
+        "neg": [[0.9, 1.9], [2.9, 3.9]],
+    }
+    temp = tempfile.NamedTemporaryFile(delete=False, suffix='.pkl')
+    with open(temp.name, 'wb') as f:
+        pickle.dump(dummy_data, f)
+    return temp.name
+
+  def test_triplet_dataset_length():
+    path = create_dummy_pickle()
+    try:
+        dataset = TripletDataset(path)
+        assert len(dataset) == 2
+    finally:
+        os.remove(path)
+
+  path = create_dummy_pickle()
+  try:
+    ds = TripletDataset(path)
+    q, p, n = ds[0]
+
+    assert isinstance(q, torch.Tensor)
+    assert isinstance(p, torch.Tensor)
+    assert isinstance(n, torch.Tensor)
+    assert q.shape == p.shape == n.shape == torch.Size([2])
+    assert q.dtype == p.dtype == n.dtype == torch.float32
+
+    dl = torch.utils.data.DataLoader(dataset=ds, batch_size=3)
+    ex = next(iter(dl))
+    print(ex)
+
+  finally:
+    os.remove(path)
+  
+  
